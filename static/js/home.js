f22aa14945dbfbbe769a9563101f5928c2948144
thanhtruong0349 <88703477+thanhtruong0349@users.noreply.github.com>
2023-06-18 16:29:55 +0700

    Name

--- static/js/home.js
+++
@@ -1,6 +0,0 @@
-let services =  document.querySelector(".w3-container");
-
-// Used to toggle the menu on small screens when clicking on the menu button
-function myFunction() {
-  services.scrollIntoView(true);
-  }
